9e9f3e087fad7a9a3917b40d5cf00b7b66d0e85e
Tamerlan Zhabassov <zhbssv@mail.ru>
2022-07-28 01:26:34 +0600

    4

--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task2;
 
 public class NamingConvention {
     public static void main(String[] args) {
-        int _variable = 10;
+        int variable = 10;
         int variableOne = 1;
         int variableTwo = 2;
         int variableThree = 3;
@@ -10,6 +10,6 @@ public class NamingConvention {
         final int VARIABLE_FIVE = 5;
         int variableSix = 6;
         int variableSeven = 7;
-        int VariableEight = 8;
+        int variableEight = 8;
     }
 }
